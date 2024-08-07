4a4a232a856fe0aac642aced34074c05a6de5d71
callan321 <callan.mitchell11@gmail.com>
2024-08-07 10:35:59 +1000

    New Vite React with taiwlwindcss & react router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div className="w-full flex justify-center items-center">
+            <h1 className="text-sky-300 text-center text-2xl">I Really hope this will work</h1>
+            <p className="text-center text-xl text-amber-800">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+
+    )
+}
+
