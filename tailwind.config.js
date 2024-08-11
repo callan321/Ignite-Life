4eab038e460b61ea0545be53ca4924b2c04ca3c6
callan321 <callan.mitchell11@gmail.com>
2024-08-12 06:31:42 +1000

    Added ignite colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ import forms from "@tailwindcss/forms";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ignite: {
+          white: '#ffffff',
+          light: '#f8f7f4',
+          cream: '#ddc181',
+          mid : '#bd9479',
+          blue: '#d2e3e4',
+          teal: '#dfe8de',
+          brown: '#745e4d',
+          dark: '#5b4f47',
+          black: '#020404',
+        },
+      },
+    },
   },
   plugins: [forms],
 };
