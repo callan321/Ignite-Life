4a8370aea4b98cd6e63951e0da646a9c8593249f
callan321 <callan.mitchell11@gmail.com>
2024-08-16 14:35:44 +1000

    Refactor

--- src/pages/home/HeroButton.tsx
+++ src/pages/home/HeroButton.tsx
@@ -1,28 +1,22 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-export default function HeroButton() {
+export default function HeroButton({name, onClick}: { name: string; onClick: () => void }) {
   const [shine, setShine] = useState(false);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setShine((prev) => !prev);
-    }, 1000);
+    }, 3000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  const handleClick = () => {
-    navigate("/contact");
-  };
-
   return (
     <button
-      onClick={handleClick}
+      onClick={onClick}
       className="relative overflow-hidden bg-[#bd9479] text-shadow shadow-button text-white hover:text-gray-100 py-3 px-6 rounded-md border-2 hover:border-gray-100 border-white/75 uppercase font-bold tracking-wide transition-all ease-in-out duration-500 hover:bg-[#b5896f] hover:shadow-button-xl hover:scale-105"
     >
-      <span className="relative z-10">Button</span>
+      {name}
       <span
         className={`absolute top-0 left-0 w-[150%] h-full bg-gradient-to-r from-transparent via-white/20 to-transparent opacity-0 pointer-events-none transform ${
           shine ? "shine" : ""
--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,16 +1,6 @@
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import styles from "./herosectionstyles.module.css";
 import HeroButton from "./HeroButton.tsx";
-
-interface HeroSlideProps {
-  title: string;
-  content: string;
-  button: string;
-  href: string;
-  ImageNow: string;
-  ImagePrev: string;
-}
+import { useNavigate } from "react-router-dom";
 
 interface HeroSectionProps {
   title: string;
@@ -20,95 +10,65 @@ interface HeroSectionProps {
   backgroundImage: string;
 }
 
-function HeroSlide({
-  title,
-  content,
-  button,
-  href,
-  ImageNow,
-  ImagePrev,
-}: HeroSlideProps) {
-  return (
-    <div className="relative isolate flex h-[36rem] items-center justify-center overflow-hidden bg-ignite-cream md:h-[48rem] lg:h-[64rem]">
-      {/* Current Image */}
-      <img
-        alt=""
-        src={ImageNow}
-        className={`${styles["long-slide-in"]} absolute inset-0 -z-10 h-full w-full object-cover max-w-full `}
-      />
-
-      <img
-        alt=""
-        src={ImagePrev}
-        className={`${styles["long-slide-out"]} absolute inset-0 -z-20 h-full w-full object-cover max-w-full`}
-      />
-
-      {/* Background Overlay */}
-      <div className="absolute inset-0 -z-10 bg-black opacity-25"></div>
-
-      {/* Main Content */}
-      <div
-        className={`${styles["late-fade-out"]} mx-auto max-w-2xl py-32 sm:py-48 lg:py-56`}
-      >
-        <div className="-mt-8 space-y-6 text-center md:space-y-8">
-          <h1
-            className={`${styles["shadow-text"]} ${styles["hero-in"]} text-4xl font-bold tracking-tight text-white sm:text-6xl`}
-          >
-            {title}
-          </h1>
-
-          <p
-            className={`${styles["shadow-text"]} ${styles["hero-in-d1"]} text-xl font-medium leading-8 text-gray-100`}
-          >
-            {content}
-          </p>
-
-          <div
-            className={`${styles["hero-in-d2"]} flex items-center justify-center gap-x-6`}
-          >
-            <Link to={href} className={styles["hero-button"]}>
-              {button}
-            </Link>
-            <HeroButton/>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 export default function HeroSection({
-  sections,
-}: {
+                                      sections,
+                                    }: {
   sections: HeroSectionProps[];
 }) {
   const [currentSection, setCurrentSection] = useState(0);
   const [previousSection, setPreviousSection] = useState<number | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const interval = setInterval(() => {
       setPreviousSection(currentSection);
       setCurrentSection((prevSection) =>
-        prevSection === sections.length - 1 ? 0 : prevSection + 1,
+        prevSection === sections.length - 1 ? 0 : prevSection + 1
       );
-    }, 15000);
+    }, 10000);
 
     return () => clearInterval(interval);
   }, [currentSection, sections.length]);
 
   return (
-    <HeroSlide
-      key={currentSection}
-      title={sections[currentSection].title}
-      content={sections[currentSection].content}
-      button={sections[currentSection].button}
-      href={sections[currentSection].href}
-      ImageNow={sections[currentSection].backgroundImage}
-      ImagePrev={
-        previousSection !== null
-          ? sections[previousSection].backgroundImage
-          : ""
-      }
-    />
+    <div className="relative isolate flex h-[36rem] items-center justify-center overflow-hidden bg-ignite-cream md:h-[48rem] lg:h-[64rem]">
+      {/* Current Image */}
+      <img
+        alt=""
+        src={sections[currentSection].backgroundImage}
+        className="absolute inset-0 fade-in -z-10 h-full w-full object-cover max-w-full"
+      />
+
+      {/* Previous Image */}
+      {previousSection !== null && (
+        <img
+          alt=""
+          src={sections[previousSection].backgroundImage}
+          className="absolute inset-0 fade-out -z-20 h-full w-full object-cover max-w-full"
+        />
+      )}
+
+      {/* Background Overlay */}
+      <div className="absolute inset-0 -z-10 bg-black opacity-25"></div>
+
+      {/* Main Content */}
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="-mt-8 space-y-6 text-center text-shadow md:space-y-8">
+          <h1 className="text-4xl font-bold tracking-tight slide-in text-white sm:text-6xl">
+            {sections[currentSection].title}
+          </h1>
+
+          <p className="text-xl slide-in-typing typing font-medium leading-8 text-shadow text-gray-100">
+            {sections[currentSection].content}
+          </p>
+          <div className="flex slide-in items-center justify-center gap-x-6">
+            <HeroButton
+              name={sections[currentSection].button}
+              onClick={() => navigate(sections[currentSection].href)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
